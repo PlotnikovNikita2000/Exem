e5176db19ea5e8b73cc66986e0e5925fac8d9e9e
unamee08 <plotnikov.nikitos2014@mail.ru>
2020-04-25 13:36:53 +0400

    Написан код ввода с строк с клавиатуры, описан класс Tour

---
+++ src/Tour/Tour/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour
+{
+    class Program
+    {
+        public static int arrSize = 0;
+        public static Tours[] tur;
+        static void Main(string[] args)
+        {
+            tur = new Tours[arrSize];
+            Console.WriteLine("Длина массива: " + tur.Length);
+            for (int i = 0; i < tur.Length; i++)
+            {
+                Console.WriteLine("Заполните элемент массива");
+                tur[i] = new Tours();
+                Console.WriteLine("Введите название поездки");
+                string poezdka = Console.ReadLine();
+
+                tur[i].Poezdka = poezdka;
+
+                Console.WriteLine("Введите продолжительность поездки");
+                string prodol = Console.ReadLine();
+
+
+                tur[i].Prodol = prodol;
+
+                Console.WriteLine("Введите цену");
+                string price = Console.ReadLine();
+
+
+                tur[i].Price = price;
+            }
+            Console.WriteLine("Массив");
+            foreach (Tours b in tur)
+            {
+                Console.WriteLine(b.Poezdka);
+                Console.WriteLine(b.Prodol);
+                Console.WriteLine(b.Price);
+                Console.WriteLine();
+            }
+
+        }
+
+        
+}
---
+++ src/Tour/Tour/Tours.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour
+{
+    class Tours
+    {
+        public string poezdka;
+        public string prodol;
+        public string price;
+
+        public Tours()
+        {
+            this.poezdka = "";
+            this.prodol = "";
+            this.price = "";
+        }
+
+        public string Poezdka
+        {
+            get { return this.poezdka; }
+            set { this.poezdka = value; }
+        }
+        public string Prodol
+        {
+            get { return this.prodol; }
+            set { this.prodol = value; }
+        }
+        public string Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+    }
+}
